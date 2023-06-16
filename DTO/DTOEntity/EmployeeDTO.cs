0b637512fb1df8ca793a13c9635efadf4554d4e8
ArizCefDev <arizcefdev@gmail.com>
2023-06-16 21:30:45 +0400

    Add project files.

---
+++ DTO/DTOEntity/EmployeeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.DTOEntity
+{
+	public class EmployeeDTO
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public double Salary { get; set; }
+	}
+}
---
+++ WebAPI_9F/Controllers/FirstController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_9F.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
